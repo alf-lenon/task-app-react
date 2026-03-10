758ef87526d3faef2ca670e395eb0a78edd3b430
Alf <sample@example.com>
2026-03-10 11:24:33 +0800

    feat: format task deadlines using dayjs

--- react-task-app/src/components/RenderTask.jsx
+++ react-task-app/src/components/RenderTask.jsx
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+
 // Display Tasks
 // This is where we call deleteTask
 function RenderTask({ task, status, user, deadline, id, deleteTask }) {
@@ -7,7 +9,7 @@ function RenderTask({ task, status, user, deadline, id, deleteTask }) {
 				Task: {task} <button onClick={() => deleteTask(id)}>Delete</button>
 			</p>
 			<p>Assigned to: {user}</p>
-			<p>Deadline: {deadline}</p>
+			<p>Deadline: {dayjs(deadline).format('MMMM, D, YYYY')}</p>
 			<span>Status: {status}</span>
 		</div>
 	);
